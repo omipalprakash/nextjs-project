0d470f23f64e4611b34d43041f7647814a38375c
omipalprakash <omprakshpal739@gmail.com>
2026-01-11 19:37:55 +0530

    Fix  ddjjf

--- pvc-pipes-website/next.config.ts
+++ pvc-pipes-website/next.config.ts
@@ -2,20 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true, // ✅ Strapi Cloud के लिए OK
+    unoptimized: true, // 🔥 IMPORTANT
     remotePatterns: [
-      // 🔹 LOCAL STRAPI (development)
       {
         protocol: "http",
         hostname: "localhost",
         port: "1337",
-        pathname: "/uploads/**",
-      },
-
-      // 🔹 STRAPI CLOUD (production)
-      {
-        protocol: "https",
-        hostname: "splendid-boot-3cb33a55a4.media.strapiapp.com",
         pathname: "/uploads/**",
       },
     ],
